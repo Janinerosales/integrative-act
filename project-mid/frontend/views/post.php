d2a6c68c9cf23d9d6c3901e4161d8314e116a587
Janinerosales <rhosalej@gmail.com>
2024-04-25 13:39:45 +0800

    first commit

---
+++ project-mid/frontend/views/post.php
@@ -0,0 +1,27 @@
+<?php include '../views/dashboard.php' ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Simple Table Post</title>
+        <link rel="stylesheet" href="../public/assets/css/post.css">
+    </head>
+    <body>
+
+    <div class="post">
+        <table>
+            <tr>
+                <th>Title</th>
+                <td>Malieee</td>
+            </tr>
+            <tr>
+                <th>Content</th>
+                <td>Let It Shine!</td>
+            </tr>
+        </table>
+    </div>
+
+    </body>
+    </html>
+  
---
+++ project-mid/frontend/views/user.php
@@ -0,0 +1,46 @@
+<?php include '../views/dashboard.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>User Table</title>
+<link rel="stylesheet" href="../public//assets/css/user.css">
+
+<body>
+
+<h2>User Table</h2>
+
+<table>
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Email</th>
+      <th>Role</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Janine Rosales</td>
+      <td>rhosalesj@gmail.com</td>
+      <td>Admin</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Jhaydee Rosales</td>
+      <td>jhaydee@example.com</td>
+      <td>User</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Michael Johnson</td>
+      <td>michael@example.com</td>
+      <td>User</td>
+    </tr>
+  </tbody>
+</table>
+
+</body>
+</html>
